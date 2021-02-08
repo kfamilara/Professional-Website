52ccc52a03bdf9fae43be37a1f02ee7088fc6837
kfamilara <77161319+kfamilara@users.noreply.github.com>
2021-02-07 21:03:55 -0500

    Addition to Website

--- Personal Website/app.js
+++ Personal Website/app.js
@@ -5,6 +5,7 @@ const menuLinks = document.querySelector(".navbar__menu");
 const mobileMenu = () => {
     menu.classList.toggle('is_active');
     menuLinks.classList.toggle('active');
+    menuLinks.style.transition = "transform 0.4s ease-in-out";
 
 };
 
@@ -21,7 +22,7 @@ const nextBtn = document.querySelector("#nextBtn");
 // counter
 
 let counter = 1;
-const size = carouselImages[0].clientWidth;
+const size = 284.5;
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
@@ -29,7 +30,7 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 nextBtn.addEventListener('click', () => {
     if (counter >= 4) return;
-    carouselSlide.style.transition = "transform 1s ease-in-out";
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
@@ -37,7 +38,7 @@ nextBtn.addEventListener('click', () => {
 
 prevBtn.addEventListener('click', () => {
     if (counter <=0) return;
-    carouselSlide.style.transition = "transform 1s ease-in-out";
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
@@ -47,32 +48,118 @@ carouselSlide.addEventListener("transitionend", () => {
     if (carouselImages[counter].id === "lastcopy") {
         carouselSlide.style.transition = "none";
         counter = 3;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
     if (carouselImages[counter].id === "firstcopy") {
         carouselSlide.style.transition = "none";
         counter = 1;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 })
 
 // Education Appear
 
+const carousel_container = document.querySelector(".carousel_container")
 const educationbox = document.querySelector('.education');
 const educationoption = document.querySelector('#education');
+const skillsoption = document.querySelector('#skills');
+const hobbyoption = document.querySelector('#hobbypicture');
+
+let width = carousel_container.clientWidth;
+
+main = document.querySelector(".main")
 
 const educationboxmove = () => {
-educationbox.style.transition = "transform 1s ease-in-out";
-educationbox.style.transform = 'translateX(0px)';
+// let width = carousel_container.clientWidth;
+// if (width == 284.5) { 
+educationbox.style.transition = "all 0.4s ease-in-out";
+carousel_container.classList.toggle('active'); 
+educationbox.classList.toggle("active");
+main.classList.toggle("active");
+
+
 };
 
 const educationboxmoveback = () => {
-    educationbox.style.transition = "transform 1s ease-in-out";
-    educationbox.style.transform = 'translateX(-284.5px)';
-    };
+    educationbox.style.transition = "all 0.4s ease-in-out";
+    educationbox.classList.toggle("active");
+    carousel_container.classList.toggle('active');
+    main.classList.toggle("active");
+
+
+};
     
 educationoption.addEventListener('click', educationboxmove);
 educationbox.addEventListener('click', educationboxmoveback);
+// educationbox.addEventListener('mouseleave', educationboxmoveback);
+// Above does not really work on mobile, cuz there is no hover.
+
+
+const blurb = document.querySelector(".blurb")
+
+
+blurb.addEventListener("mousemove", (e) => {
+    let xAxis = (150 - e.layerX)/5;
+    console.log(e);
+    let yAxis = (131 - e.layerY)/5;
+    blurb.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
+    blurb.style.transition = "all 0.4s ease"
+
+});
+
+blurb.addEventListener("mouseleave", (e) => {
+    blurb.style.transform = `none`;
+    blurb.style.transition = "all 0.4s ease"
+
+});
+
+// Why does this not work? Ok now it works
+
+
+educationoption.addEventListener("mousemove", (e) => {
+    let xAxis = (130 -e.layerX)/3;
+    console.log(e);
+    let yAxis = (143-e.layerY)/3;
+    educationoption.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
+
+});
+
+educationoption.addEventListener("mouseleave", (e) => {
+
+    educationoption.style.transform = `none`;
+    educationoption.style.transition = "all 0.4s ease"
+
+});
+
+//Skills Button!
+const skills = document.querySelector("#skills");
+
+
+
+
+
+
+//slider
+
+// slider= document.querySelector("#sliders");
+output =document.querySelector('#demo');
+// output.innertext = slider.value;
+
+// slider.addEventListener(oninput, (e) => {
+// output.innerhtml = e.value;
+// output.innerhtml = e.value; 
+
+// });
+
+// slider.oninput = function() {
+//     output.innerHTML = e.value;
+// };
+graphs = document.querySelector (".graphs")
+
+output = document.querySelector('#demo');
+function slider (value) {
+graphs.style.transform = 'translateX(' + (-100 * value) + '%)';
+}
 
 
 
